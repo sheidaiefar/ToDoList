9166f00aae8ae8f8d74aae65a474b82a176d0e9b
333 <sheyda@yahoo.com>
2020-01-15 12:14:55 +0330

    app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'todo';
+  list = [];
+  text = '';
+  @ViewChild('input', null) el: ElementRef;
+
+  ngOnInit(): void {
+    this.el.nativeElement.focus();
+  }
+
+  onCheck(index) {
+    var icon = this.list[index].icon == 'bx:bx-radio-circle' ? 'fe:check-circle-o' : 'bx:bx-radio-circle'
+    var temp = { id: index, text: this.list[index].text, icon: icon, checked: !this.list[index].checked }
+    this.list.splice(index, 1, temp);
+  }
+  onItemAdd() {
+    this.text != '' ? this.list.push({ id: this.list.length, text: this.text, icon: 'bx:bx-radio-circle', checked: false }) : '';
+    this.text = '';
+    this.el.nativeElement.focus();
+  }
+  onDelete(index) {
+    this.list.splice(index, 1)
+  }
+  onRemoveCheckedItems() {
+    var temp = this.list.filter(s => s.checked == false);
+    this.list = temp;
+    this.el.nativeElement.focus();
+  }
 }
+
